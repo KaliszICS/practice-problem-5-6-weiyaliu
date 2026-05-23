8abc1ba3114e37faca63ccda56dae7d46b190e44
weiyaliu <343445839@tdsb.ca>
2026-05-23 19:19:26 -0400

    Implement methods for age retrieval, length array, and swapping
    Added methods to get age by name, create an array of lengths, and swap two people in the arrays.

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,62 @@ public class PracticeProblem {
 
 	}
 
+	//1.
+	public static int getAge (String[] names, int[] ages, String name) {
+	    
+	    int age = -1;
+	    
+	    for (int i = 0; i < names.length; i++) {
+	        
+	         (name.equal(names[i])) {
+	            age = ages[i];
+	        }
+	    }
+	    
+	    return age;
+	}
+	
+	//2.
+	public static int[] createLengthArrary (String[] arr) {
+	    
+	    int[] lengths = new int(arr.length);
+	    
+	    for (int i= o; i < arr.length; i++) {
+	        lengths[i] = arr[i].length();
+	    }
+	    
+	    return lengths;
+	}
+	
+	//3.
+	public static void swapPeople (String[] names, int[] ages, String name1, String name2) {
+	    
+	    int index1 = -1;
+	    int index2 = -1;
+	    
+	    for (int i= 0; i < names.length; i++) {
+	        
+	        if (names1.equals(names[i])) {
+	            index1 = i;
+	        }
+	        
+	        if (names2.equals(names[i])) {
+	            index2 = i;
+	        }
+	        
+	   String tempName;
+	   
+	   tempName = names[index1];
+	   names[index1] = names[index2];
+	   names[index2] = tempName;
+	   
+	   int tempAge;
+	   
+	   tempAge = ages[index1];
+	   ages[index1] = ages[index2];
+	   ages[index2] = tempAge;
+	        
+	    }
+	}
+
 }
